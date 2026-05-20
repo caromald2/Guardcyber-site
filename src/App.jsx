1fc4cd5d6230c306c319ce9330d1302eb481d04a
caromald2 <caromald2@gmail.com>
2026-05-20 22:42:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
-
+export default function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#0B1120",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "Arial"
+    }}>
+      <h1>GuardCyber is Live 🚀</h1>
+    </div>
+  )
+}
